a458fcc2f52417fbc581af24fab751263a51f049
RobySP <robysyahp@gmail.com>
2017-05-29 14:02:33 +0700

    beranda

---
+++ home.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+    <title> </title>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <style>
+        header {
+            background-color: orange;
+        }
+
+    </style>
+</head>
+
+<body>
+    <header>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="apple-itunes-app" content="app-id=1147053983">
+        <meta name="google-play-app" content="app-id=sg.gov.nlb.nlbmoblie">
+
+        <div class="container">
+            <div class="row">
+                <div id="logo" class="navbar-header">
+                    <a href="#">
+                        <img src="img/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="navbar-top">'
+
+                </div>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
