d5a9a5d3e323f2bff65548c2e81df6f06c8df14c
RobySP <robysyahp@gmail.com>
2017-05-29 15:04:53 +0700

    header

--- home.php
+++ home.php
@@ -18,15 +18,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="apple-itunes-app" content="app-id=1147053983">
         <meta name="google-play-app" content="app-id=sg.gov.nlb.nlbmoblie">
-
         <div class="container">
             <div class="row">
-                <div id="logo" class="navbar-header">
+                <div id="logo" class="span6 add-bottom">
                     <a href="#">
                         <img src="img/logo.png" alt="">
                     </a>
                 </div>
-                <div class="navbar-top">'
+                <div id="dpklampung" class="span6 text-right except-phone">
+                    <a href="#" title="Lampung" class="clearfix" target="_blank">
+                        <img src="img/Lampung.png" width="200px" height="90px">
+                    </a>
+                    <a class="dropdown">
+                        <button class="btn-default dropdown-toggle" type="button" id="situs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Situs ini
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="situs">
+                            <li><a href="#">Situs ini</a></li>
+                            <li><a href="#">Perpusnas</a></li>
+                            <li><a href="#">OneSearch</a></li>
+                        </ul>
+                    </a>
+
+                    </ul>
+
+
 
                 </div>
             </div>
